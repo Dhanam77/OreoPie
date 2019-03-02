2930c5a3ae1f1398b2dd3c58a0934cb1b3ee9e07
Dhanam <parekhdhanam@gmail.com>
2019-03-02 14:25:43 +0530

    changed UI

--- app/src/main/java/com/example/oreopie/SignUpActivity.java
+++ app/src/main/java/com/example/oreopie/SignUpActivity.java
@@ -2,7 +2,6 @@ package com.example.oreopie;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.media.Image;
